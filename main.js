05c159a2742cbb6d26c060235ac3f60409ea88af
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2023-08-31 11:09:31 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const taskInput = document.getElementById('task-input')
+const addButton = document.getElementById('add-btn')
+const taskList = document.getElementById('task-list')
+// add from
+addButton.addEventListener('click', ()=>{
+    const taskText = taskInput.value.trim()
+    
+    if(taskText !==''){
+        const listItem = document.createElement('li')
+        const taskSpan = document.createElement('span')
+        taskSpan.textContent = taskText
+        taskSpan.classList.add('task-text')
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+
+        deleteButton.classList.add('delete-button')
+        listItem.appendChild(taskSpan)
+        listItem.appendChild(deleteButton)
+        taskList.appendChild(listItem)
+        saveData(taskList)
+        taskInput.value = ''
+    }
+})
+
+// delete
+taskList.addEventListener('click', (event)=> {
+    if(event.target.classList.contains('delete-button')){
+        const listItem = event.target.parentNode;
+        taskList.removeChild(listItem);
+        saveData(taskList)
+    }
+})
+
+const saveData = (taskList) => {    
+    localStorage.setItem('data', taskList.innerHTML)
+}
+
+// add from local storage
+
+const showTask = () => {
+    const li = localStorage.getItem('data')
+    taskList.innerHTML = li;
+} 
+
+showTask();
